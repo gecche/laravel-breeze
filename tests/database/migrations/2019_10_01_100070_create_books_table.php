efadfd46cfb1a4e70a0923e55920f7ecc2bc724f
Giacomo Terreni <giacomo.terreni@gmail.com>
2020-02-03 11:13:08 +0100

    started tests

---
+++ tests/database/migrations/2019_10_01_100070_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+
+/**
+ * Class Posts
+ */
+class CreateBooksTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('language');
+            $table->integer('author_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+
+}
