d5130371699279393fed6c3d0027c35df6048df2
Giacomo Terreni <giacomo.terreni@gmail.com>
2020-02-12 11:36:31 +0100

    make:migration command tests

--- tests/src/BreezeMakeMigrationCommandTestCase.php
+++ tests/src/BreezeMakeMigrationCommandTestCase.php
@@ -24,12 +24,35 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
 {
 
 
-
     protected $migrationsDir;
     protected $laravelAppPath;
 
-    protected $migrationNames;
+    protected $migrationsToMake = [
+        'create_pippo_table' => [
+            '--create' => true,
+            '--table' => 'pippo',
+            '--ownerships' => 'yes',
+            '--timestamps' => 'yes',
+        ],
+        'create_pippo2_table' => [
+            '--create' => true,
+            '--table' => 'pippo2',
+            '--ownerships' => 'null',
+            '--timestamps' => 'null',
+        ],
+        'create_pippo3_table' => [
+            '--create' => true,
+            '--table' => 'pippo3',
+            '--ownerships' => 'no',
+            '--timestamps' => 'no',
+        ],
+        'update_blank_table' => [
 
+        ],
+        'update_pippo_table' => [
+            '--table' => 'pippo',
+        ],
+    ];
 
 
     use RefreshDatabase;
@@ -45,17 +68,11 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
         $this->laravelAppPath = __DIR__ . '/../../vendor/orchestra/testbench-core/laravel';
         $this->migrationsDir = $this->laravelAppPath . '/database/migrations';
 
-        $this->migrationNames = [
-            'create_pippo_table',
-            'create_pippo2_table',
-        ];
-
         parent::setUp();
 
 
-        //$this->cleanMigrations();
         $this->beforeApplicationDestroyed(function () {
-            //$this->cleanMigrations();
+            $this->cleanMigrations();
         });
 
     }
@@ -69,12 +86,6 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
     protected function getEnvironmentSetUp($app)
     {
         // set up database configuration
-        $app['config']->set('breeze',
-            [
-//                'default-models-dir' => $this->modelsDir,
-//                'namespace' => $this->modelsNamespace,
-            ]
-        );
 
     }
 
@@ -96,27 +107,93 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
     /*
      * TEST #1 FOR MAKE MIGRATION COMMAND
      *
+     * We test that Breeze's migration:make command creates migration files accordingly to the various
+     * options found
+     *
+     *
      */
-    public function testMakeMigrationCommand() {
+    public function testMakeMigrationCommand()
+    {
 
 
-        $filesToCheck = $this->getMigrationFiles($this->migrationNames);
-        $this->assertEquals([],$filesToCheck);
+        $filesToCheck = $this->getMigrationFiles(array_keys($this->migrationsToMake));
+        $this->assertEquals([], $filesToCheck);
 
-        foreach ($this->migrationNames as $migrationName) {
-            $this->artisan('make:migration',
-                [
-                    'name' => $migrationName,
-                    '--table' => 'pippo',
-                    '--ownerships' => 'yes',
-                    '--timestamps' => 'null',
-                    '--create' => true,
-                ]
-            );
+        foreach ($this->migrationsToMake as $migrationName => $migrationParams) {
+            $migrationParams = array_merge(['name' => $migrationName], $migrationParams);
+
+            $this->artisan('make:migration', $migrationParams);
         }
 
-        $filesToCheck = $this->getMigrationFiles($this->migrationNames);
-        $this->assertEquals(count($this->migrationNames),count($filesToCheck));
+        $filesToCheck = $this->getMigrationFiles(array_keys($this->migrationsToMake));
+        $this->assertEquals(count($this->migrationsToMake), count($filesToCheck));
+
+        $useStringsToCheck = [
+            "use Gecche\\Breeze\\Facades\\Schema;",
+            "use Gecche\\Breeze\\Database\\Schema\\Blueprint;",
+        ];
+
+        $timestampsStringsTocheck = [
+            'null' => '$table->nullableTimestamps();',
+            'no' => '$table->nullableTimestamps();',
+        ];
+
+        foreach ($this->migrationsToMake as $migrationName => $migrationParams) {
+
+            $files = $this->getMigrationFiles($migrationName);
+
+            $stringsToCheck = $useStringsToCheck;
+            $stringsShouldNotContains = [];
+
+            if (array_key_exists('--create', $migrationParams)) {
+                $timestamps = Arr::get($migrationParams, '--timestamps', 'yes');
+                switch ($timestamps) {
+                    case 'no':
+                        $stringsShouldNotContains[] = '$table->nullableTimestamps();';
+                        $stringsShouldNotContains[] = '$table->timestamps();';
+                        break;
+                    case 'null':
+                        $stringsShouldNotContains[] = '$table->timestamps();';
+                        $stringsToCheck[] = '$table->nullableTimestamps();';
+                        break;
+                    default:
+                        $stringsShouldNotContains[] = '$table->nullableTimestamps();';
+                        $stringsToCheck[] = '$table->timestamps();';
+                        break;
+                }
+                $ownerships = Arr::get($migrationParams, '--ownerships', 'no');
+                switch ($ownerships) {
+                    case 'no':
+                        $stringsShouldNotContains[] = '$table->nullableOwnerships();';
+                        $stringsShouldNotContains[] = '$table->ownerships();';
+                        break;
+                    case 'null':
+                        $stringsShouldNotContains[] = '$table->ownerships();';
+                        $stringsToCheck[] = '$table->nullableOwnerships();';
+                        break;
+                    default:
+                        $stringsShouldNotContains[] = '$table->nullableOwnerships();';
+                        $stringsToCheck[] = '$table->ownerships();';
+                        break;
+                }
+
+
+            }
+
+            foreach ($files as $file) {
+
+
+                $fileContents = File::get($this->migrationsDir . '/' . $file);
+                foreach ($stringsToCheck as $stringToCheck) {
+                    $this->assertContains($stringToCheck, $fileContents);
+                }
+                foreach ($stringsShouldNotContains as $stringToCheck) {
+                    $this->assertNotContains($stringToCheck, $fileContents);
+                }
+            }
+
+
+        }
 
 //        $this->artisan('breeze:relations',['model' => 'Author']);
 //       print_r(Artisan::output());
@@ -125,27 +202,10 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
     }
 
 
+    protected function cleanMigrations()
+    {
 
-
-    protected function removeRelationTraitUse($filename,$modelName) {
-
-        if (!File::exists($filename)) {
-            return;
-        }
-
-        $fileContent = File::get($filename);
-
-        $relationTraitUseString = "\n\t"."use Relations\\".$modelName."Relations;\n";
-
-        $fileContent = str_replace($relationTraitUseString,'',$fileContent);
-
-        File::put($filename,$fileContent);
-
-    }
-
-    protected function cleanMigrations() {
-
-        $files = $this->getMigrationFiles($this->migrationNames);
+        $files = $this->getMigrationFiles(array_keys($this->migrationsToMake));
 
         foreach ($files as $filename) {
             File::delete($this->migrationsDir . '/' . $filename);
@@ -154,19 +214,20 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
     }
 
 
-    protected function getMigrationFiles($suffixes) {
-        $suffixes = Arr::wrap($suffixes);
+    protected function getMigrationFiles($migrationNames)
+    {
+        $migrationNames = Arr::wrap($migrationNames);
 
 
-        $files = array_filter(File::files($this->migrationsDir),function ($file) use ($suffixes) {
+        $files = array_filter(File::files($this->migrationsDir), function ($file) use ($migrationNames) {
 
             $regEx = '/';
 
-            foreach ($this->migrationNames as $migrationName) {
-                $regEx .= '(.*)'.$migrationName.'\.php$|';
+            foreach ($migrationNames as $migrationName) {
+                $regEx .= '(.*)' . $migrationName . '\.php$|';
             }
 
-            $regEx = rtrim($regEx,'|').'/U';
+            $regEx = rtrim($regEx, '|') . '/U';
 
 
             return preg_match($regEx, $file->getRelativePathname());
@@ -174,7 +235,7 @@ class BreezeMakeMigrationCommandTestCase extends \Orchestra\Testbench\TestCase
 
         return array_map(function ($el) {
             return $el->getRelativePathname();
-        },$files);
+        }, $files);
 
     }
 
